452ba6656d92f1d8c9c79b6687e353c19858a7c9
Manjeet <manjeet9871124@gmail.com>
2026-04-06 14:46:48 +0530

    manjeet commit

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,30 +1,55 @@
 import nodemailer from "nodemailer";
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
+// export const sendOTPEmail = async (email, otp) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     await transporter.sendMail({
+//       from: `"Voxy Live" <${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Your OTP Code",
+//       html: `<h2>Your OTP is: ${otp}</h2>`,
+//     });
+
+//   } catch (err) {
+//     console.error("EMAIL ERROR:", err.message);
+//     throw new Error("Email sending failed");
+//   }
+// };
+
+
 export const sendOTPEmail = async (email, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
     await transporter.sendMail({
       from: `"Voxy Live" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Your OTP Code",
       html: `<h2>Your OTP is: ${otp}</h2>`,
     });
-
   } catch (err) {
     console.error("EMAIL ERROR:", err.message);
     throw new Error("Email sending failed");
