0f2556ecdec13f7e4607303e1fec6e7261657f97
Manjeet <manjeet9871124@gmail.com>
2026-04-06 16:19:40 +0530

    manjeet commit

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,33 +1,67 @@
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendOTPEmail = async (email, otp) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     await transporter.sendMail({
+//       from: `"Voxy Live" <${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Your OTP Code",
+//       html: `<h2>Your OTP is: ${otp}</h2>`,
+//     });
+
+//   } catch (err) {
+//     console.error("EMAIL ERROR:", err.message);
+//     throw new Error("Email sending failed");
+//   }
+// };
+
+// ############# NEW CODE BY MANJEET ############# 
+
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 export const sendOTPEmail = async (email, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
     await transporter.sendMail({
-      from: `"Voxy Live" <${process.env.EMAIL_USER}>`,
+      from: `"Voxy Live" <${process.env.SMTP_USER}>`,
       to: email,
       subject: "Your OTP Code",
-      html: `<h2>Your OTP is: ${otp}</h2>`,
+      html: `
+        <h2>Your OTP is: ${otp}</h2>
+        <p>Valid for 30 seconds</p>
+      `,
     });
 
+    console.log("✅ OTP SENT");
+
   } catch (err) {
-    console.error("EMAIL ERROR:", err.message);
+    console.error("❌ EMAIL ERROR:", err);
     throw new Error("Email sending failed");
   }
 };
-
